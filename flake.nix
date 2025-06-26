fc250a9c21ee5497859caee3c7ad49b067a6cb4e
Cameron Leslie <cameronleslie@gmail.com>
2025-06-26 08:50:29 -0700

    maybe

--- flake.nix
+++ flake.nix
@@ -4,12 +4,12 @@
   outputs = {self}: {
     templates = {
       # Generic
-      default.path = ./template/default;
-      default.description = "Generic Development Flake + Tooling";
+      generic.path = ./template/default;
+      generic.description = "Generic Development Flake + Tooling";
       # Deno
       deno.path = ./template/deno;
       deno.description = "Deno 2 Flake + Tooling";
     };
-    defaultTemplate = self.templates.default;
+    defaultTemplate = self.templates.generic;
   };
 }
